ba7f105aa838a7c12b910775cba56b5f3dc7f3c1
Ali eren demir <a.e.demir2020@gmail.com>
2025-10-18 23:05:47 +0300

    Elimdeki C projelerini ekledim

---
+++ code/gün1/döngülerveteçift.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int sayi;
+    printf("Bir sayi gir: ");
+    scanf("%d", &sayi);
+
+    if (sayi % 2 == 0)
+        printf("Sayi cift.\n");
+    else
+        printf("Sayi tek.\n");
+
+    printf("1'den %d'ye kadar sayilar:\n", sayi);
+    for (int i = 1; i <= sayi; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
---
+++ code/gün1/sayıfarklarıvetoplamı.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    printf("ilk sayiyi girin: ");
+    scanf("%d",&x);
+    printf("\nikinci sayiyi girin: ");
+    scanf("%d",&y);
+
+    printf("\niki sayinin toplami=%d",x+y);
+    printf("\niki sayinin farki=%d",x-y);
+
+
+    return 0;
+}
